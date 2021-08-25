3acd0735e76585f9f7560d41483c99b5b3fbc8b7
Rob R <82601224+mr-flowjangles@users.noreply.github.com>
2021-08-25 12:44:52 -0500

    Update load_tbl.sql

--- Oracle-Multi-Thread/load_tbl.sql
+++ Oracle-Multi-Thread/load_tbl.sql
@@ -5,15 +5,4 @@ create table LOAD_A_THREAD
   message  VARCHAR2(100),
   chunk    NUMBER,
   run_date DATE
-)
-tablespace USERS2
-  pctfree 10
-  initrans 1
-  maxtrans 255
-  storage
-  (
-    initial 80K
-    next 1M
-    minextents 1
-    maxextents unlimited
-  );
+);
