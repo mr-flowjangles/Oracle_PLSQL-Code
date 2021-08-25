a9ae9a430c367111a8f2e9cf30dab11d302c626c
Rob R <82601224+mr-flowjangles@users.noreply.github.com>
2021-08-25 12:49:21 -0500

    Update thread_control.sql

--- Oracle-Multi-Thread/thread_control.sql
+++ Oracle-Multi-Thread/thread_control.sql
@@ -10,4 +10,11 @@ create table THREAD_CONTROL
   create_date      DATE,
   create_user      VARCHAR2(30),
   update_date      DATE,
-  update_user      VARCHAR2(30));
+  update_user      VARCHAR2(30)
+)
+;
+
+
+insert into THREAD_CONTROL (job_thread_id, job_package, job_proc, job_schema, job_thread_count, is_active, job_notes, create_date, create_user, update_date, update_user)
+values (5, 'heavyThreader', 'main', user, 5, 'Y', 'Testing the first PX Threading', trunc(sysdate), user, trunc(sysdate), 'dd-mm-yyyy'), user);
+commit;
